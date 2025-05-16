dbeee7fc4741c7118ca36d54af5f65e4319212d7
Edwin Ivan S <68554673+EdwinIvanS@users.noreply.github.com>
2025-05-16 15:38:22 -0500

    microservicio-buscador Productos

---
+++ ProyectoTiendaGuitarrasBackend/microservice-product/src/test/java/com/buscadorservice/microservice_product/MicroserviceProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buscadorservice.microservice_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
