6be502c11ffab7cb320a6fa901ee9dcd25e8418e
Edwin Ivan S <68554673+EdwinIvanS@users.noreply.github.com>
2025-05-17 08:46:20 -0500

    microservicio-operador Ordenes

---
+++ ProyectoTiendaGuitarrasBackend/microservice_orders/src/test/java/com/operadorservice/microservice_orders/MicroserviceOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.operadorservice.microservice_orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
